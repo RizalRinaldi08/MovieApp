4e822e0ce4e31559d25ae532777a14ab08491115
Muhammad Rizal Rinaldi <rrizal549@gmail.com>
2024-07-04 16:30:20 +0700

    setup env variable

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
+import { API_URL, API_ACCESS_TOKEN } from '@env'
 
 const MovieDetail = ({ navigation }: any): any => {
   const fetchData = (): void => {
     // Gantilah dengan akses token Anda
-    const ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
-
-    const url =
-      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
-
+  
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
       },
     }
 
-    fetch(url, options)
+    fetch(API_URL, options)
       .then(async (response) => await response.json())
       .then((response) => {
         console.log(response)
